61783c833a7bac9e8032c910be8b43b2c136c4f4
Ten-Dark <hromaplay844@gmail.com>
2025-07-10 17:34:40 +0300

    git init

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import {useCallback, useState} from "react";
+import {addEdge, applyEdgeChanges, applyNodeChanges, Background, Controls, ReactFlow} from "@xyflow/react";
+import '@xyflow/react/dist/style.css'
+
+const initialNodes = [
+    {
+        id: 'n1',
+        position: {x: 0, y: 0},
+        data: {label: 'Node 1'}
+    },
+    {
+        id: 'n2',
+        position: {x: 0, y: 100},
+        data: {label: 'Node 2'}
+    },
+];
+
+const initialEdges = [
+    {
+        id: 'n1-n2',
+        source: 'n1',
+        target: 'n2'
+    }
+];
+
+const App = () => {
+    const [nodes, setNodes] = useState(initialNodes);
+    const [edges, setEdges] = useState(initialEdges);
+
+    const onNodesChange = useCallback(
+        (changes) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+        [],
+    )
+
+    const onEdgesChange = useCallback(
+        (changes) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+        [],
+    )
+
+    const onConnect = useCallback(
+        (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+        [],
+    )
+
+    return (
+        <div style={{height: '100vh', width: '100vw'}}>
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                fitView
+            >
+                <Background/>
+                <Controls/>
+            </ReactFlow>
+        </div>
+    )
+}
+
+export default App
